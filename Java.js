1a47f7bcfcbc49a4ab65872977408b9c9715b6f7
aymano255 <123076561+aymano255@users.noreply.github.com>
2023-03-09 10:14:52 +0100

    Add files via upload

---
+++ Java.js
@@ -0,0 +1,6 @@
+document.getElementById("button1").addEventListener("click", function() {
+    document.getElementById("extra-square").style.display = "block";
+  });
+  document.getElementById("button2").addEventListener("click", function() {
+    document.getElementById("extra-square").style.display = "block";
+  });
